291ce8c28eaed75e26c357aa2e6ba3fbaf62f142
Usman Saeed <uusaeed749@gmail.com>
2024-03-23 22:57:16 +0500

    added register page and functionality

--- store/views.py
+++ store/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .forms import SignUpForm
 
 
 def home(request):
@@ -35,3 +39,23 @@ def logout_user(request):
     logout(request)
     messages.success(request, ("You have been logged out successfully!"))
     return redirect('home')
+
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("You have Registered successfully!"))
+            return redirect('home')
+        else:
+            messages.success(
+                request, ("There was an error, Please try again!"))
+            return redirect('register')
+    else:
+        return render(request, 'register.html', {'form': form})

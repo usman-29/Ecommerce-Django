b6a1b2ce720174825057d50923785e74ba76d5f2
Usman Saeed <uusaeed749@gmail.com>
2024-03-24 22:37:12 +0500

    added product.html page to show unique product

--- store/views.py
+++ store/views.py
@@ -13,6 +13,11 @@ def home(request):
     return render(request, 'home.html', {'products': products})
 
 
+def product(request, pk):
+    product = Product.objects.get(id=pk)
+    return render(request, 'product.html', {'product': product})
+
+
 def about(request):
     return render(request, 'about.html', {})
 

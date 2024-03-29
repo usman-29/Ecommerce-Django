0330cd0be7f674b15458e6970344614c7abba170
Usman Saeed <uusaeed749@gmail.com>
2024-03-29 22:57:53 +0500

    added jquery ajax in product.html

--- cart/cart.py
+++ cart/cart.py
@@ -6,3 +6,11 @@ class Cart():
         if 'session_key' not in request.session:
             cart = self.session['session_key'] = {}
         self.cart = cart
+
+    def add(self, product):
+        product_id = str(product.id)
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id] = {'price': str(product.price)}
+        self.session.modified = True

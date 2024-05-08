dc307410b5891d5d4bf70137cd44709ac84f5f43
Usman Saeed <uusaeed749@gmail.com>
2024-05-08 18:04:27 +0500

    resolved the update quantity issue in cart summary page. Also added total price of items in cart.

--- cart/views.py
+++ cart/views.py
@@ -6,9 +6,12 @@ from django.contrib import messages
 
 
 def cart_summary(request):
+   # Get the cart
     cart = Cart(request)
-    cart_products = cart.get_prod()
-    return render(request, 'cart_summary.html', {'cart_products': cart_products})
+    cart_products = cart.get_prods
+    quantities = cart.get_quants
+    totals = cart.cart_total()
+    return render(request, "cart_summary.html", {"cart_products": cart_products, "quantities": quantities, "totals": totals})
 
 
 def cart_add(request):

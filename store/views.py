e9ea2ca186729ceeb5f5d68bfa9b170925c8e01b
Usman Saeed <uusaeed749@gmail.com>
2024-05-10 20:40:18 +0500

    user will fill out user info while registering and also added update info functionality

--- store/views.py
+++ store/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
-from .models import Product, Category
+from .models import Product, Category, Profile
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
-from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm
+from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm, UserInfoForm
 
 
 def home(request):
@@ -72,8 +72,9 @@ def register_user(request):
             password = form.cleaned_data['password1']
             user = authenticate(username=username, password=password)
             login(request, user)
-            messages.success(request, ("You have Registered successfully!"))
-            return redirect('home')
+            messages.success(
+                request, ("Username Created - Please Fill Out User Info Below"))
+            return redirect('update_info')
         else:
             messages.success(
                 request, ("There was an error, Please try again!"))
@@ -121,4 +122,21 @@ def update_password(request):
             return render(request, "update_password.html", {'form': form})
     else:
         messages.success(request, "You Must Be Logged In To View That Page...")
+        return redirect('home')
+
+
+def update_info(request):
+    if request.user.is_authenticated:
+        current_user = Profile.objects.get(user__id=request.user.id)
+        form = UserInfoForm(request.POST or None, instance=current_user)
+
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, "Your Info Has Been Updated!!")
+            return redirect('home')
+        return render(request, "update_info.html", {'form': form})
+    else:
+        messages.success(
+            request, "You Must Be Logged In To Access That Page!!")
         return redirect('home')
